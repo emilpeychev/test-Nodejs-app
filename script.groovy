350283dcff32a4ff0a54894bc42f6b311891d802
emilpeychev <emil.peychev@gmail.com>
2024-03-24 12:28:33 +0200

    add push to dockerHub

--- script.groovy
+++ script.groovy
@@ -6,14 +6,14 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'docker build -t test/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push softunium/demo-app:$(date +"%Y%m%d%H%M%S")'
     }
 }
 
-// def deployApp() {
-//     echo 'deploying the application...'
-// }
+def deployApp() {
+    echo 'deploying the application...'
+}
 
 return this
